35c31ed18cb63c4407e9b6f1fb99926f0c5a749a
Jack.Serjeant@googlemail.com <dookie_chumo@hotmail.com>
2016-01-29 23:06:32 +0000

    First Commit
    First Commit

---
+++ Ritual/Assets/Scripts/littleMenController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class littleMenController : MonoBehaviour {
+    public Rigidbody2D rb;
+    public EdgeCollider2D col;
+    bool isColWithSlope;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        isColWithSlope = false;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate()
+    {
+        if (isColWithSlope == false)
+        {
+            rb.AddForce(new Vector2(-10, 0));
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "slope")
+        {
+            isColWithSlope = true;
+            rb.AddForce(new Vector2(0, -10));
+        }
+        /*if (col.gameObject.name == "slope")
+        {
+            Debug.Log("HELLO");
+            isColWithSlope = true;
+            rb.AddForce(new Vector2(0, 0));
+            rb.isKinematic = true;
+        }*/
+    }
+}
