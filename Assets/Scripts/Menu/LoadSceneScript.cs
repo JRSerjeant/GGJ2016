3055368dd38e782e1e96d21b94cb82533a46e534
William Lord <william@williamlord.co.uk>
2016-01-30 12:03:10 +0000

    Menu and credits scenes

---
+++ Assets/Scripts/Menu/LoadSceneScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneScript : MonoBehaviour {
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
