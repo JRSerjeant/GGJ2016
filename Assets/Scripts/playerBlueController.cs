9c6f46686f39e57fa887bbd6b9f9dfd5f806ccda
Jack Serjeant <jack.serjeant@gmail.com>
2016-01-30 07:23:06 +0000

    Added player arrows with controllers

---
+++ Assets/Scripts/playerBlueController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerBlueController : MonoBehaviour {
+    public int speed;
+    public GameObject blueTopArrow;
+    public GameObject blueSideArrow;
+    // Use this for initialization
+    void Start () {
+        speed = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey("a"))
+        {
+            blueTopArrow.transform.position += new Vector3(-speed, 0, 0) * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            blueTopArrow.transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
+        }
+        if (Input.GetKey("w"))
+        {
+            blueSideArrow.transform.position += new Vector3(0, speed, 0) * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            blueSideArrow.transform.position += new Vector3(0, -speed, 0) * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/playerRedController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerRedController : MonoBehaviour {
+
+    public int speed;
+    public GameObject redTopArrow;
+    public GameObject redSideArrow;
+    // Use this for initialization
+    void Start()
+    {
+        speed = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey("left"))
+        {
+            redTopArrow.transform.position += new Vector3(-speed, 0, 0) * Time.deltaTime;
+        }
+        if (Input.GetKey("right"))
+        {
+            redTopArrow.transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
+        }
+        if (Input.GetKey("up"))
+        {
+            redSideArrow.transform.position += new Vector3(0, speed, 0) * Time.deltaTime;
+        }
+        if (Input.GetKey("down"))
+        {
+            redSideArrow.transform.position += new Vector3(0, -speed, 0) * Time.deltaTime;
+        }
+    }
+}
