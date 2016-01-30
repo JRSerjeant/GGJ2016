d22d76b09660d03967b1dd35d2cf6f308943ba3f
Jack Serjeant <jack.serjeant@gmail.com>
2016-01-30 00:41:57 +0000

    Added folders.

--- Assets/Scripts/littleMenController.cs
+++ Assets/Scripts/littleMenController.cs
@@ -1,32 +1,68 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class littleMenController : MonoBehaviour {
+public class littleMenController : MonoBehaviour
+{
     public Rigidbody2D rb;
     public EdgeCollider2D col;
+
     bool isColWithSlope;
+    bool atEndPoint;
+
+    Vector2 nw;
+
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
         isColWithSlope = false;
+        atEndPoint = false;
     }
-	
-	// Update is called once per frame
-	void FixedUpdate()
+
+    // Update is called once per frame
+    void Update()
     {
+
+    }
+
+
+    void FixedUpdate()
+    {
+        if (transform.position.x < 0.0f)
+        {
+            Debug.Log(transform.position.x);
+            atEndPoint = true;
+        }
+        if (atEndPoint == true)
+        {
+            Debug.Log("Should Destroy");
+            Destroy(gameObject);
+        }
+
+        nw = new Vector2(-1, 0);
+
         if (isColWithSlope == false)
         {
-            rb.AddForce(new Vector2(-10, 0));
+            rb.velocity = nw.normalized;
+            //rb.AddForce(new Vector2(-10, 0));
         }
     }
 
     void OnCollisionEnter2D(Collision2D col)
     {
+        if (col.gameObject.name == "slopeEnd")
+        {
+            isColWithSlope = false;
+        }
+
         if (col.gameObject.name == "slope")
         {
+            Debug.Log("HELLO");
+            nw = new Vector2(-1, 1);
             isColWithSlope = true;
-            rb.AddForce(new Vector2(0, -10));
+            rb.velocity = nw.normalized;
+            //rb.AddForce(new Vector2(0, 100));
         }
         /*if (col.gameObject.name == "slope")
         {
---
+++ Assets/Scripts/playerOneCreateLittleMen.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerOneCreateLittleMen : MonoBehaviour
+{
+    public GameObject men;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+        
+
+    public IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(1.0f);
+        Instantiate(men, new Vector2(8.0f, -1.3f), new Quaternion());
+        yield return new WaitForSeconds(1.0f);
+    }
+}
