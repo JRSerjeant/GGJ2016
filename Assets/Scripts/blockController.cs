b9802164c34d23af9ad701951e3a3b811559e3bd
Jack.Serjeant@googlemail.com <dookie_chumo@hotmail.com>
2016-01-30 18:37:40 +0000

    Added block count
    Signed-off-by: Jack.Serjeant@googlemail.com <dookie_chumo@hotmail.com>

--- Assets/Scripts/blockController.cs
+++ Assets/Scripts/blockController.cs
@@ -5,20 +5,34 @@ public class blockController : MonoBehaviour {
     public GameObject block;
     public GameObject bluePlayerTop;
     public GameObject redPlayerTop;
+    public int redPlayerBlockCount;
+    public int bluePlayerBlockCount;
+    public int blockCount;
+
     // Use this for initialization
     void Start () {
-	
+        blockCount = 10;
+        redPlayerBlockCount = 0;
+        bluePlayerBlockCount = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            GameObject go = Instantiate(block,bluePlayerTop.GetComponent<Renderer>().transform.position , new Quaternion()) as GameObject;
+            if (bluePlayerBlockCount < blockCount)
+            {
+                bluePlayerBlockCount ++;
+                GameObject go = Instantiate(block, bluePlayerTop.GetComponent<Renderer>().transform.position, new Quaternion()) as GameObject;
+            }
         }
         if (Input.GetKeyDown(KeyCode.RightControl))
         {
-            GameObject go = Instantiate(block, redPlayerTop.GetComponent<Renderer>().transform.position, new Quaternion()) as GameObject;
+            if (redPlayerBlockCount < blockCount)
+            {
+                redPlayerBlockCount ++;
+                GameObject go = Instantiate(block, redPlayerTop.GetComponent<Renderer>().transform.position, new Quaternion()) as GameObject;
+            }
         }
     }
 }
