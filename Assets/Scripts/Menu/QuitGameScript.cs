b8efc782144cfd08ff7e87da7439f8721badd2f8
William Lord <william@williamlord.co.uk>
2016-01-30 14:09:09 +0000

    Background audio

--- Assets/Scripts/Menu/QuitGameScript.cs
+++ Assets/Scripts/Menu/QuitGameScript.cs
@@ -1,10 +1,21 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class QuitGameScript : MonoBehaviour
 {
     public void Quit()
     {
+        StartCoroutine(playSoundThenLoad());
+    }
+
+    IEnumerator playSoundThenLoad()
+    {
+        var audioSource = GetComponent<AudioSource>();
+
+        audioSource.Play();
+        yield return new WaitForSeconds(audioSource.clip.length);
+
         Application.Quit();
     }
 }
