d68b8065e1434072d5f3b8b260818234e3ed16e5
Jack.Serjeant@googlemail.com <dookie_chumo@hotmail.com>
2016-01-30 14:12:25 +0000

    We have a score system.

---
+++ Assets/Scripts/scoreController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scoreController : MonoBehaviour {
+    private static int redPlayerScore;
+    private static int bluePlayerScore;
+
+    // Use this for initialization
+    void Start () {
+        redPlayerScore = 0;
+        bluePlayerScore = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.ClearDeveloperConsole();
+        Debug.Log("Red: " + redPlayerScore);
+        Debug.Log("Blue: " + bluePlayerScore);
+	}
+    public static void addRedPlayerScore()
+    {
+        redPlayerScore ++;
+    }
+    public static void addBluePlayerScore()
+    {
+        bluePlayerScore++;
+    }
+}
