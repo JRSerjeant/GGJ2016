f0c59997340860dca50a1045cf0568296e537abc
JRSerjeant <Jack.Serjeant@gmail.com>
2016-04-25 16:51:56 +0100

    Added script to auto build and copy to DropBox

---
+++ Assets/Editor/ScriptBatch.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System;
+using System.IO;
+
+public class ScriptBatch
+{
+    [MenuItem("MyTools/Windows Build With Postprocess")]
+    public static void BuildGame()
+    {
+        // Get filename.
+        string path = "C:/builds/";
+        string date = DateTime.Now.ToString("HHss_dd_MM_yyyy");
+        string fullpath = path + date;
+        Directory.CreateDirectory(fullpath);
+
+        //string path = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
+        string[] levels = new string[] { "Assets/Scenes/main.unity" };
+
+
+
+
+        // Build player.
+        Debug.Log("Starting Build");
+        BuildPipeline.BuildPlayer(levels, fullpath + "/build.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+
+        // Copy a file from the project folder to the build folder, alongside the built game.
+        //FileUtil.CopyFileOrDirectory("Assets/WebPlayerTemplates/Readme.txt", path + "Readme.txt");
+
+        // Run the game (Process class from System.Diagnostics).
+        //Process proc = new Process();
+        //proc.StartInfo.FileName = path + "BuiltGame.exe";
+        //proc.Start();
+    }
+}
