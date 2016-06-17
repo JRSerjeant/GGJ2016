162a97fb91bf1f408715cdf0b70876dbf4736669
Jack Serjeant <Jack.Serjeant@gmail.com>
2016-06-17 23:11:09 +0100

    Fixed men not being destroyed

--- Assets/Scripts/Managers/objectFactory.cs
+++ Assets/Scripts/Managers/objectFactory.cs
@@ -24,7 +24,7 @@ public class objectFactory : MonoBehaviour
         pdf_ManGroundCollider = (GameObject)Resources.Load("_LittleMen/Prefabs/pdf_ManGroundCollider");
         pfb_Ball = (GameObject)Resources.Load("_Ball/Prefabs/Ball");
         pfb_Cog = (GameObject)Resources.Load("_Cog/Prefabs/pfb_Cog");
-        pfb_bloodParticle = (GameObject)Resources.Load("Particles/pfb_bloodParticle");
+        pfb_bloodParticle = (GameObject)Resources.Load("_Blood/Prefabs/pfb_bloodParticle");
         pfb_DisplayTextNumber = (GameObject)Resources.Load("Prefabs/Objects/pfb_DisplayTextNumber");
     }
 
---
+++ Assets/Scripts/Menu/QuitGameScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuitGameScript : MonoBehaviour
+{
+    public void Quit()
+    {
+        StartCoroutine(playSoundThenLoad());
+    }
+
+    IEnumerator playSoundThenLoad()
+    {
+        var audioSource = GetComponent<AudioSource>();
+
+        audioSource.Play();
+        yield return new WaitForSeconds(audioSource.clip.length);
+
+        Application.Quit();
+    }
+}
