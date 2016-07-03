5a7e0e07c16a67e83b553345ec1fdd0de036ba03
Jack Serjeant <Jack.Serjeant@gmail.com>
2016-07-03 10:10:18 +0100

    Fixed some positioning.

--- Assets/Objects/_Exit/Scripts/scr_Exit.cs
+++ Assets/Objects/_Exit/Scripts/scr_Exit.cs
@@ -43,6 +43,6 @@ public class scr_Exit : MonoBehaviour {
     void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
-        Gizmos.DrawSphere(transform.position,0.25f);
+        Gizmos.DrawSphere(transform.position,10f);
     }
 }
