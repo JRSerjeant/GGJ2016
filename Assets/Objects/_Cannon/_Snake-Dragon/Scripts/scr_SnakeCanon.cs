def667308eb05c74091019ced926a6173ac36201
manicmario82 <marioklos@gmail.com>
2016-07-05 02:55:05 +0100

    Clean Up
    Merging _Cog with _Snake-Dragon, Fixing order and animations

---
+++ Assets/Objects/_Cannon/_Snake-Dragon/Scripts/scr_SnakeCanon.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Assets.Scripts;
+using System.Collections;
+
+public class scr_SnakeCanon : MonoBehaviour
+{
+    public GameObject myCog;
+    // Use this for initialization
+    void Awake()
+    {
+        //myCog = objectFactory.createCog();
+        //myCog.transform.position = new Vector3(transform.position.x - 0.084f, transform.position.y + 0.027f);
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //myCog.transform.position = new Vector3(transform.position.x - 0.084f, transform.position.y + 0.027f);
+    }
+}
