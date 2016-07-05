3f05cdf6d1eee4dd48580364f1530e831fd4a367
manicmario82 <marioklos@gmail.com>
2016-07-05 05:32:36 +0100

    My first scripts (and last ones)
    In Game view change resolution/aspect ratio - game will always fill the
    screen, keeping proportions. Turn off the script and see the difference
    :wink:

---
+++ Assets/Objects/_Cameras and Lights/Scripts/PixelDensityCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class PixelDensityCamera : MonoBehaviour {
+
+	public float pixelsToUnits = 1;
+
+	void Update () {
+
+		GetComponent<Camera>().orthographicSize = Screen.height / pixelsToUnits / 2;
+	}
+}
